dd520c12500df55f612d00623579cc8aa288895e
Ando <iamando@tuta.io>
2024-04-27 13:09:23 +0300

    feat: handle new promise execution for driver and return as value driver from selenium (#18)* feat: add prettierrc json config and update git ignore* feat: add import map* docs: use contributing and code of conduct* feat: install selenium webdriver and std assert on deps* feat: configure deno.json use fmt, lint, compiler options* chore: delete tsconfig* feat: create browser engines, types, constants* feat: create gh workflows to auto create pull request* feat: add edge to browser enginer and remove utils folder from import maps* feat: clean arch and update deno fmt, lint* feat: re-arch pkg to be drowser and clean file, dir* feat: handle drowser json config and validate url* feat: handle new promise execution for driver and return as value driver from selenium (#19)
    * feat: add prettierrc json config and update git ignore
    
    * feat: add import map
    
    * docs: use contributing and code of conduct
    
    * feat: install selenium webdriver and std assert on deps
    
    * feat: configure deno.json use fmt, lint, compiler options
    
    * chore: delete tsconfig
    
    * feat: create browser engines, types, constants
    
    * feat: create gh workflows to auto create pull request
    
    * feat: add edge to browser enginer and remove utils folder from import maps
    
    * feat: clean arch and update deno fmt, lint
    
    * feat: re-arch pkg to be drowser and clean file, dir
    
    * feat: handle drowser json config and validate url
    
    * feat: handle new promise execution for driver and return as value driver from selenium
    
    * fix: handle return promise in driver in get url

--- lib.ts
+++ lib.ts
@@ -1,6 +1,6 @@
 import driver from '@pkg/driver.ts'
 
-await driver({ browserType: 'chrome' }).then((builder) => {
+driver({ browserType: 'chrome' }).then((builder) => {
 	console.log('Builder ->', builder)
 }).catch((error) => {
 	console.log('Error ->', error)
