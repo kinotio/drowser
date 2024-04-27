3fb97e5211107baaf5e6238b5e80f5a7cff64a0c
Ando <iamando@tuta.io>
2024-04-27 13:25:26 +0300

    chore: clean lib and move basic example to examples folder

--- lib.ts
+++ lib.ts
@@ -1,9 +1,3 @@
 import driver from '@pkg/driver.ts'
 
-driver({ browserType: 'chrome' }).then((builder) => {
-	console.log('Builder ->', builder)
-}).catch((error) => {
-	console.log('Error ->', error)
-})
-
 export { driver }
