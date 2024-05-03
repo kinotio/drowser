4e4de5a431ef4fec9497c3d1394f863ba1bff64c
Ando <iamando@tuta.io>
2024-05-03 18:23:50 +0300

    feat: handle creating pdf reports and check if has result before doing the process (#44)
    * feat: add prettierrc json config and update git ignore
    
    * feat: add import map
    
    * docs: use contributing and code of conduct
    
    * feat: install selenium webdriver and std assert on deps
    
    * feat: configure deno.json use fmt, lint, compiler options
    
    * chore: delete tsconfig
    
    * feat: create browser engines, types, constants
    
    * feat: create gh workflows to auto create pull request
    
    * feat: add edge to browser enginer and remove utils folder from import maps
    
    * feat: clean arch and update deno fmt, lint
    
    * feat: re-arch pkg to be drowser and clean file, dir
    
    * feat: handle drowser json config and validate url
    
    * feat: handle new promise execution for driver and return as value driver from selenium
    
    * fix: handle return promise in driver in get url
    
    * chore: clean lib and move basic example to examples folder
    
    * feat: create pkg types and handle builder quit outside the pkg for now
    
    * feat: create service type, obj to set extra services for pkg and create types
    
    * docs: update readme docs
    
    * docs: update readme docs
    
    * docs: update readme docs
    
    * feat: set version to v0.1.0
    
    * fix: for todo specify builder.quit to be able to quit internaly
    
    * docs: update docs, don't use hard tabs on readme
    
    * feat: migrate export log , pdf to be at drowser json config
    
    * feat: update return type of driver and check on finally to generate log or pdf
    
    * feat: handle builder inside driver core and get only json of test or function
    
    * chore: rename lib to be mod
    
    * feat: install kya and show loading spinner while running test and failed test
    
    * feat: handle selenium webdriver errors
    
    * feat: handle log on each level of test
    
    * feat: get results from each test to be in array of results with test status
    
    * feat: use chrome driver only on basic examples
    
    * feat: create log file and write each row of test passed or failed
    
    * feat: set export log to default a true and remove from json config
    
    * feat: generate pdf from results and install pdf-lib
    
    * feat: handle creating pdf reports and check if has result before doing the process

--- deps.ts
+++ deps.ts
@@ -1,25 +1,28 @@
 // @deno-types="npm:@types/selenium-webdriver@^4.1.22"
 import { Browser, Builder } from 'selenium-webdriver'
 import type { ThenableWebDriver } from 'selenium-webdriver'
+import { jsPDF } from 'jspdf'
+import 'jspdf-autotable'
 
 // @deno-types="https://cdn.skypack.dev/@types/lodash?dts"
 import { isEmpty } from 'lodash'
-import { PDFDocument } from 'pdf-lib'
 
 import * as assert from 'assert'
+
 import { join } from 'mod'
 import { existsSync } from 'fs'
-
 import Kia from 'kia'
+import { createWriteStream } from 'node-fs'
 
 export {
 	assert,
 	Browser,
 	Builder,
+	createWriteStream,
 	existsSync,
 	isEmpty,
 	join,
+	jsPDF,
 	Kia,
-	PDFDocument,
 	ThenableWebDriver,
 }
