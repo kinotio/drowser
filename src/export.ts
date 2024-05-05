67abc36ebbe9624e8b23b1ae4e492c08b21c1fd9
Ando <iamando@tuta.io>
2024-05-05 17:56:42 +0300

    feat: add actual and exceptation to be added to log and pdf export

--- src/export.ts
+++ src/export.ts
@@ -1,8 +1,9 @@
 import { existsSync, join, jsPDF } from '@deps'
 import { generateFileName } from '@pkg/utils.ts'
+import { TDataResult } from '@pkg/types.ts'
 
 const exportGeneratedLog = (
-	{ results }: { results: Array<{ [key: string]: any }> },
+	{ results }: { results: Array<TDataResult> },
 ): void => {
 	const dirPath = join(Deno.cwd(), 'drowser/logs')
 	const hasDir = existsSync(dirPath)
@@ -17,7 +18,8 @@ const exportGeneratedLog = (
 
 		const writeResult = () =>
 			results.forEach((r) => {
-				const logRow = `[${r.timestamp}] - Test with ${r.name} is ${r.status}`
+				const logRow =
+					`[${r.timestamp}] - Test with ${r.name} is ${r.status} with actual value is ${r.actual} and excepted to be ${r.exceptation}`
 				Deno.writeTextFile(logFilePath, `${logRow}\n`, { append: true })
 			})
 
@@ -31,7 +33,7 @@ const exportGeneratedLog = (
 }
 
 const exportGeneratedPdf = (
-	{ results }: { results: Array<{ [key: string]: any }> },
+	{ results }: { results: Array<TDataResult> },
 ): void => {
 	const dirPath = join(Deno.cwd(), 'drowser/pdfs')
 	const asDir = existsSync(dirPath)
@@ -44,8 +46,18 @@ const exportGeneratedPdf = (
 	if (Array.isArray(results) && results.length > 0) {
 		const pdf = new jsPDF()
 		const pdfCopy = Object.assign({}, pdf) as any
-		const head = [['ID', 'NAME', 'STATUS', 'TIMESTAMP']]
-		const body = results.map((r, i) => [i + 1, r.name, r.status, r.timestamp])
+		const head = [[
+			'ID',
+			'NAME',
+			'ACTUAL',
+			'EXCEPTATION',
+			'STATUS',
+			'TIMESTAMP',
+		]]
+		const body = results.map((
+			r,
+			i,
+		) => [i + 1, r.name, r.actual, r.exceptation, r.status, r.timestamp])
 		const tableOpts = { head, body, theme: 'grid' }
 
 		pdf.setFontSize(18)
